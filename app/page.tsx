0602b0f220308aa6367990863649b393af20dea0
Dayton Rumbold <45398901+Dayton-His-Glory-Alone@users.noreply.github.com>
2025-02-20 17:05:52 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [message, setMessage] = useState("");
+  const [showStory, setShowStory] = useState(false); // State to manage story visibility
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -72,7 +73,7 @@ export default function Home() {
           height={400}
           className="rounded-lg shadow-lg"
         />
-   <p className={`text-center text-lg max-w-prose ${roboto.className}`}>
+        <p className={`text-center text-lg max-w-prose ${roboto.className}`}>
           Create a habit of locking in scripture. Our diverse learning methods, including flashcards, interactive exercises, typing practice, and voice mode, ensure effective retention.
         </p>
         {/* Video Section */}
@@ -82,7 +83,6 @@ export default function Home() {
             Your browser does not support the video tag.
           </video>
         </div>
-
 
         <h1 className={`text-4xl font-bold mt-4 ${roboto.className}`} style={{ color: "#006400" }}>
           Start your journey now...
@@ -164,6 +164,23 @@ export default function Home() {
               className="hover:scale-105 transition-transform"
             />
           </a>
+        </div>
+
+        {/* Story Behind Verse Vault */}
+        <div className="w-full max-w-md mt-8 text-center">
+          <button
+            onClick={() => setShowStory(!showStory)}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Learn about the story behind Verse Vault
+          </button>
+          {showStory && (
+            <div className="mt-4 p-4 bg-white rounded-lg shadow-md">
+              <p className="text-gray-700">
+                "My wife and I wanted to make a habit of memorizing scripture together so we started something we called 'Monday Memory' where we would spend 10 minutes going over a passage. My goal was to make those nights fun and engaging. It started well but slowly we lost track of the verses we already had memorized and the new passages were getting more difficult. I wanted a tool that could 1. keep track of what I've memorized and remind me to refresh and 2. make it fun. With those goals, I started working on Verse Vault. After a couple failed attempts I finally got a working app that is something I like to use for myself now. I would like to share that with others and hope it encourages you as well."
+              </p>
+            </div>
+          )}
         </div>
       </main>
       {/* Footer */}
